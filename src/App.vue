195594861d7724b201e359330e77ffec06c979b9
originalDreamr <original.dreamr@gmail.com>
2021-07-27 21:00:53 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header title="not abdul" />
+    <Header />
+    <router-link 
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <!-- <p>anything you can change</p> -->
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+      title: {
+          type: String,
+          default: 'Hello Abdul',
+      },
+  },
+}
+</script>
+
+
+<style scoped>
+h1 {
+  margin: 40px 0 0 ;
+  font-family: "Times New Roman", Times, serif;
+}
+</style>
